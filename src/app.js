b003b24fe6a3d2c3a175daff25162b45aff33e08
Zohaib Ali <62861582+szali3@users.noreply.github.com>
2022-05-01 15:08:20 -0400

    removed dummy expesnse data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1651417195182 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000, createdAt: 1651417195182 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1651417195182 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
